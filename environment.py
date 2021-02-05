77901fa11c27dc8155e801335703835cd4b3de56
Magnus <magberd@stud.ntnu.no>
2021-02-05 16:21:25 +0100

    added docstring to Env class

--- environment.py
+++ environment.py
@@ -1,6 +1,17 @@
 from matplotlib.patches import Polygon
 
 class Env():
+      
+  """Class representing an environment.
+
+  Args:
+      entrance_point (ndarray): 2-by-0 numpy array containing (x, y)-coords of 
+      environment entrance point.
+
+      obstacle_corners (list, optional): List of n-by-2 numpy arrays. Each numpy array contains 
+      the corners of a polygon. It is assumed that there is a line between the 
+      first corner and the last corner. Defaults to [].
+  """
 
   def __init__(self, entrance_point, obstacle_corners = []):
     self.obstacle_corners = obstacle_corners
