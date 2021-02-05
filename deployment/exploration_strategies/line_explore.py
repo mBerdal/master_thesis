fixed wring sign for force in line_deployment notes and test some simple line deployment simulations

deployment/exploration_strategies/line_explore.py
@@ -6,8 +6,9 @@ from deployment.exploration_strategies.exploration_strategy import (
 import numpy as np
 
 class LineExplore(ExplorationStrategy):
-  def __init__(self, force_threshold=0.01):
+  def __init__(self, force_threshold=0.001, RSSI_threshold=0.1):
     self.force_threshold = force_threshold
+    self.RSSI_threshold = RSSI_threshold
 
   def prepare_exploration(self, target):
       return super().prepare_exploration(target)
@@ -16,7 +17,10 @@ class LineExplore(ExplorationStrategy):
     x_is = np.array([b.pos[0] for b in beacons])
     RSSIs = np.array([MIN.get_RSSI(b) for b in beacons])
     k_is = np.ones(len(beacons))
-    F = np.sum(k_is*(MIN.pos[0] - x_is - RSSIs))
-    if F < self.force_threshold:
+    k_is[-1]=10
+    F = -np.sum(k_is*(MIN.pos[0] - x_is - RSSIs))
+    if np.linalg.norm(F) < self.force_threshold:
+      x_star = np.sum(k_is*(x_is + RSSIs))/np.sum(k_is)
+      print(x_star, MIN.pos[0])
       raise AtLandingConditionException
     return np.array([F, 0])

main.py
@@ -7,6 +7,8 @@ from deployment.following_strategies.attractive_follow import AttractiveFollow
 from deployment.following_strategies.straight_line_follow import StraightLineFollow
 from deployment.exploration_strategies.potential_fields_explore import PotentialFieldsExplore
 from deployment.exploration_strategies.heuristic_explore import HeuristicExplore
+from deployment.following_strategies.no_follow import NoFollow
+from deployment.exploration_strategies.line_explore import LineExplore
 from deployment.deployment_fsm import DeploymentFSM
 
 import numpy as np
@@ -24,7 +26,9 @@ def simulate(dt, mins, scs, env):
     beacons = np.append(beacons, m)
     for b in beacons:
       b.compute_neighbors(beacons)
-    print(f"min {m.ID} landed at pos\t\t\t {m.pos}\nits target now has {len(m.deployment_strategy.get_target().neighbors)} neighs\n------------------", )
+    print(f"min {m.ID} landed at pos\t\t\t {m.pos}")
+    if not m.deployment_strategy.get_target() is None:
+          print(f"Its target now has {len(m.deployment_strategy.get_target().neighbors)} neighs\n------------------", )
   print(f"minimum number of neighbors: {min(beacons, key=lambda b: len(b.neighbors))}")    
 
 if __name__ == "__main__":
@@ -39,25 +43,18 @@ if __name__ == "__main__":
     obstacle_corners = [
       np.array([
         [-10, -10],
-        [ 10, -10], 
-        [ 10,  10],
-        [-10,  10]
-      ]),
-      np.array([
-        [-5, -5],
-        [ 5, -5], 
-        [ 5,  5],
-        [-5,  5]
+        [ -6, -10], 
       ]),
     ]
   )
 
   max_range = 3
 
-  N_mins = 24
+  N_mins = 15
   dt = 0.01
 
   scs = SCS(max_range)
+  """ Potential fields exploration
   mins = [
     Min(
       max_range,
@@ -71,6 +68,18 @@ if __name__ == "__main__":
           K_o=1,
           min_force_threshold=0.1
         )
+      )
+    ) for _ in range(N_mins)
+  ]
+  """
+  """ Line exploration """
+
+  mins = [
+    Min(
+      max_range,
+      DeploymentFSM(
+        NoFollow(),
+        LineExplore()
       )
     ) for _ in range(N_mins)
   ]